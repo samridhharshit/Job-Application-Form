c7adc30c868d0e036b18131d05df6fdeb8058f16
samridhharshit <samridhharsh@gmail.com>
2019-07-05 00:00:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// opening and closing navigation bar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
